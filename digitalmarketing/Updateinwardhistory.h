98a2950c1dba49adcef65be169ea77755a25fa18
kaushik <kaushik.movaliya48@gmail.com>
2017-05-21 13:54:05 +0530

    resolve changes

--- digitalmarketing/Updateinwardhistory.h
+++ digitalmarketing/Updateinwardhistory.h
@@ -10,22 +10,37 @@
 
 @interface Updateinwardhistory : UIViewController
 {
-    NSDictionary *OrderDetailDict,*OrderProductDict;
+    NSDictionary *customerDict;
+    NSMutableArray *ProductArry;
+    NSString *CutomerID;
+    
+    NSInteger totalAmount;
+    NSInteger totalQTY;
+    NSString *ProductJSONString;
+    BOOL CheckSUCCESS;
 
 }
+@property (weak, nonatomic) IBOutlet UIButton *SelectCutomer_Button;
+@property (weak, nonatomic) IBOutlet UIView *SelectCustomerBackView;
+@property (weak, nonatomic) IBOutlet UIView *SearchProBackView;
+@property (weak, nonatomic) IBOutlet UIView *ProductTitleBackView;
+@property (weak, nonatomic) IBOutlet UIButton *CreateOrderBtn;
+@property (weak, nonatomic) IBOutlet UITableView *CustomerTBL;
+@property (weak, nonatomic) IBOutlet UIView *CutomerView;
+@property (weak, nonatomic) IBOutlet UITableView *ProductTBL;
+@property (weak, nonatomic) IBOutlet UILabel *TotalQTY_LBL;
+@property (weak, nonatomic) IBOutlet UILabel *TotalAmount_LBL;
+@property (weak, nonatomic) IBOutlet UILabel *GrantTotal_LBL;
+@property (weak, nonatomic) IBOutlet UILabel *Discount_LBL;
+@property (weak, nonatomic) IBOutlet UILabel *CutomerNameLBL;
+@property (weak, nonatomic) IBOutlet UILabel *CustomerAdressLBL;
+@property (weak, nonatomic) IBOutlet UILabel *CustomerStateCityLBL;
+@property (weak, nonatomic) IBOutlet UILabel *CustomerPhoneLBL;
 
-@property (strong, nonatomic) NSString *inward_id;
+@property (strong, nonatomic) IBOutlet NSLayoutConstraint *TitleTop1;
+@property (strong, nonatomic) IBOutlet NSLayoutConstraint *TitleTop2;
+@property (strong, nonatomic) IBOutlet NSLayoutConstraint *TitleTop3;
+@property (strong, nonatomic) IBOutlet NSLayoutConstraint *TitleTop4;
+@property (strong, nonatomic) IBOutlet NSLayoutConstraint *TitleHight;
 
-@property (weak, nonatomic) IBOutlet UIView *DetailBackView;
-@property (weak, nonatomic) IBOutlet UIView *TitleBackView;
-@property (weak, nonatomic) IBOutlet UITableView *OrderDetailTable;
-
-@property (weak, nonatomic) IBOutlet UILabel *CutomerName;
-@property (weak, nonatomic) IBOutlet UILabel *OrderStatus;
-@property (weak, nonatomic) IBOutlet UILabel *OrderNamuber;
-@property (weak, nonatomic) IBOutlet UILabel *OrderDate;
-@property (weak, nonatomic) IBOutlet UILabel *TotalQTY;
-@property (weak, nonatomic) IBOutlet UILabel *TotalAmount;
-@property (weak, nonatomic) IBOutlet UILabel *Discount;
-@property (weak, nonatomic) IBOutlet UILabel *GrandTotal;
 @end
--- digitalmarketing/Updateinwardhistory.m
+++ digitalmarketing/Updateinwardhistory.m
@@ -8,169 +8,374 @@
 
 #import "Updateinwardhistory.h"
 #import "digitalMarketing.pch"
-#import <QuartzCore/QuartzCore.h>
+#import "SerachProductVW.h"
 #import "OrderDetail_Cell.h"
 
-@interface Updateinwardhistory ()
+@interface Updateinwardhistory ()<SerachProductVWDelegate>
 
 @end
 
 @implementation Updateinwardhistory
-@synthesize DetailBackView,CutomerName,OrderDate,OrderStatus,OrderNamuber,TotalAmount,TotalQTY,GrandTotal,Discount;
-@synthesize TitleBackView,OrderDetailTable;
+
+@synthesize SearchProBackView,SelectCustomerBackView,ProductTitleBackView;
+@synthesize CreateOrderBtn,CutomerView,CustomerTBL,SelectCutomer_Button;
+@synthesize ProductTBL;
+@synthesize TitleTop1,TitleTop2,TitleTop3,TitleTop4,TitleHight;
+@synthesize CustomerPhoneLBL,CustomerAdressLBL,CutomerNameLBL,CustomerStateCityLBL;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    [DetailBackView.layer setCornerRadius:3.0f];
-    DetailBackView.layer.borderWidth = 1.0f;
-    [DetailBackView.layer setMasksToBounds:YES];
-    DetailBackView.layer.borderColor = [UIColor whiteColor].CGColor;
     
-    DetailBackView.layer.masksToBounds = NO;
-    [DetailBackView.layer setShadowColor:[UIColor colorWithRed:(218/255.0) green:(219/255.0) blue:(225/255.0) alpha:1.0].CGColor];
-    [DetailBackView.layer setShadowOpacity:0.8];
-    [DetailBackView.layer setShadowRadius:2.0];
-    [DetailBackView.layer setShadowOffset:CGSizeMake(0.3,0.3)];
+    CustomerPhoneLBL.text=@"";
+    CustomerAdressLBL.text=@"";
+    CutomerNameLBL.text=@"";
+    CustomerStateCityLBL.text=@"";
     
-    [TitleBackView.layer setCornerRadius:3.0f];
-    TitleBackView.layer.borderWidth = 1.0f;
-    [TitleBackView.layer setMasksToBounds:YES];
-    TitleBackView.layer.borderColor = [UIColor whiteColor].CGColor;
+    TitleTop1.constant=0;
+    TitleTop2.constant=0;
+    TitleTop3.constant=0;
+    TitleTop4.constant=0;
+    TitleHight.constant=10;
     
-    TitleBackView.layer.masksToBounds = NO;
-    [TitleBackView.layer setShadowColor:[UIColor colorWithRed:(218/255.0) green:(219/255.0) blue:(225/255.0) alpha:1.0].CGColor];
-    [TitleBackView.layer setShadowOpacity:0.8];
-    [TitleBackView.layer setShadowRadius:2.0];
-    [TitleBackView.layer setShadowOffset:CGSizeMake(0.3,0.3)];
+    CheckSUCCESS=NO;
+    [SelectCustomerBackView.layer setCornerRadius:3.0f];
+    SelectCustomerBackView.layer.borderWidth = 1.0f;
+    SelectCustomerBackView.layer.borderColor = [UIColor clearColor].CGColor;
     
+    [SelectCustomerBackView.layer setShadowColor:[UIColor colorWithRed:(218/255.0) green:(219/255.0) blue:(225/255.0) alpha:1.0].CGColor];
+    [SelectCustomerBackView.layer setShadowOpacity:0.8];
+    [SelectCustomerBackView.layer setShadowRadius:1.0];
+    [SelectCustomerBackView.layer setShadowOffset:CGSizeMake(0.3,0.3)];
+    
+    [SearchProBackView.layer setCornerRadius:3.0f];
+    SearchProBackView.layer.borderWidth = 1.0f;
+    SearchProBackView.layer.borderColor = [UIColor clearColor].CGColor;
+    [SearchProBackView.layer setShadowColor:[UIColor colorWithRed:(218/255.0) green:(219/255.0) blue:(225/255.0) alpha:1.0].CGColor];
+    [SearchProBackView.layer setShadowOpacity:0.8];
+    [SearchProBackView.layer setShadowRadius:1.0];
+    [SearchProBackView.layer setShadowOffset:CGSizeMake(0.3,0.3)];
+    
+    
+    [ProductTitleBackView.layer setCornerRadius:3.0f];
+    ProductTitleBackView.layer.borderWidth = 1.0f;
+    ProductTitleBackView.layer.borderColor = [UIColor clearColor].CGColor;
+    [ProductTitleBackView.layer setShadowColor:[UIColor colorWithRed:(218/255.0) green:(219/255.0) blue:(225/255.0) alpha:1.0].CGColor];
+    [ProductTitleBackView.layer setShadowOpacity:0.8];
+    [ProductTitleBackView.layer setShadowRadius:1.0];
+    [ProductTitleBackView.layer setShadowOffset:CGSizeMake(0.3,0.3)];
+    
+    [CreateOrderBtn.layer setCornerRadius:3.0f];
+    CreateOrderBtn.layer.borderWidth = 1.0f;
+    [CreateOrderBtn.layer setMasksToBounds:YES];
+    CreateOrderBtn.layer.borderColor = [UIColor clearColor].CGColor;
+    
+    [CutomerView.layer setCornerRadius:3.0f];
+    CutomerView.layer.borderWidth = 1.0f;
+    CutomerView.layer.borderColor = [UIColor clearColor].CGColor;
+    [CutomerView.layer setShadowColor:[UIColor grayColor].CGColor];
+    [CutomerView.layer setShadowOpacity:0.8];
+    [CutomerView.layer setShadowRadius:2.0];
+    [CutomerView.layer setShadowOffset:CGSizeMake(10,10)];
+    
+    CutomerView.hidden=YES;
+    CutomerID=@"";
     
     UINib *nib = [UINib nibWithNibName:@"OrderDetail_Cell" bundle:nil];
     OrderDetail_Cell *cell = [[nib instantiateWithOwner:nil options:nil] objectAtIndex:0];
-    OrderDetailTable.rowHeight = cell.frame.size.height;
-    [OrderDetailTable registerNib:nib forCellReuseIdentifier:@"OrderDetail_Cell"];
+    ProductTBL.rowHeight = cell.frame.size.height;
+    [ProductTBL registerNib:nib forCellReuseIdentifier:@"OrderDetail_Cell"];
+}
+
+-(void)getVender
+{
+    NSMutableDictionary *dictParams = [[NSMutableDictionary alloc] init];
+    [dictParams setObject:Base_Key  forKey:@"key"];
+    [dictParams setObject:get_vendor  forKey:@"s"];
     
-    [DetailBackView setHidden:YES];
-    [TitleBackView setHidden:YES];
-    
+    [CommonWS AAwebserviceWithURL:[NSString stringWithFormat:@"%@",BaseUrl] withParam:dictParams withCompletion:^(NSDictionary *response, BOOL success1)
+     {
+         [self handleVenderResponse:response];
+     }];
+}
+- (void)handleVenderResponse:(NSDictionary*)response
+{
+    if ([[[response objectForKey:@"ack"]stringValue ] isEqualToString:@"1"])
+    {
+        customerDict=[response valueForKey:@"result"];
+        [CustomerTBL reloadData];
+        CheckSUCCESS=YES;
+    }
+    else
+    {
+        [CustomerTBL reloadData];
+        [AppDelegate showErrorMessageWithTitle:AlertTitleError message:[response objectForKey:@"ack_msg"] delegate:nil];
+    }
+}
+- (IBAction)SelectCustomerBtn_POPUP_Action:(id)sender
+{
+    CutomerView.hidden=NO;
     BOOL internet=[AppDelegate connectedToNetwork];
     if (internet)
     {
-        [self getInwardDetial];
+        if (!CheckSUCCESS)
+        {
+            [self getVender];
+        }
+        
     }
     else
         [AppDelegate showErrorMessageWithTitle:@"" message:@"Please check your internet connection or try again later." delegate:nil];
 }
 
--(void)getInwardDetial
+#pragma mark UITableView delegate
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    
-//    NSMutableDictionary *dictParams = [[NSMutableDictionary alloc] init];
-//    [dictParams setObject:Base_Key  forKey:@"key"];
-//    [dictParams setObject:get_inward_store_item  forKey:@"s"];
-//    [dictParams setObject:self.inward_id  forKey:@"inward_id"];
-//    
-//    [CommonWS AAwebserviceWithURL:[NSString stringWithFormat:@"%@",BaseUrl] withParam:dictParams withCompletion:^(NSDictionary *response, BOOL success1)
-//     {
-//         [self handleInwardDetialResponse:response];
-//     }];
-    
-}
-
-- (void)handleInwardDetialResponse:(NSDictionary*)response
-{
-    if ([[[response objectForKey:@"ack"]stringValue ] isEqualToString:@"1"])
+    if (tableView==CustomerTBL)
     {
-        
-        OrderDetailDict=[response valueForKey:@"result"];
-        OrderProductDict=[OrderDetailDict valueForKey:@"products"];
-        
-        CutomerName.text=[OrderDetailDict valueForKey:@"vendor_name"];
-        OrderNamuber.text=[NSString stringWithFormat:@"Inward Number :#%@",[OrderDetailDict valueForKey:@"id"]];
-        OrderDate.text=[NSString stringWithFormat:@"Inward Date :%@",[OrderDetailDict valueForKey:@"adate"]];
-        TotalQTY.text=[NSString stringWithFormat:@"Nos.%@",[OrderDetailDict valueForKey:@"total_qty"]];
-        GrandTotal.text=[NSString stringWithFormat:@"Rs.%@",[OrderDetailDict valueForKey:@"grand_total"]];
-        
-        [DetailBackView setHidden:NO];
-        [TitleBackView setHidden:NO];
-        
-        [OrderDetailTable reloadData];
+        return 1;
     }
     else
     {
-        [DetailBackView setHidden:YES];
-        [TitleBackView setHidden:YES];
-        [OrderDetailTable reloadData];
-        [AppDelegate showErrorMessageWithTitle:AlertTitleError message:[response objectForKey:@"ack_msg"] delegate:nil];
+        return ProductArry.count;
     }
 }
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return OrderProductDict.count;
+    if (tableView==CustomerTBL )
+    {
+        return customerDict.count;
+    }
+    else
+    {
+        return 1;
+    }
 }
 
--(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+-(CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
 {
-    return 1;
+    if (tableView==CustomerTBL)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0.0;
+    }
+    //return 10.0f;
 }
 
-
-- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+-(UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
 {
-    return 0.0; // you can have your own choice, of course
+    //if (tableView==CustomerTBL)
+    // {
+    //  return nil;
+    // }
+    UIView *v = [UIView new];
+    [v setBackgroundColor:[UIColor clearColor]];
+    return v;
 }
-
-- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
-{
-    UIView *headerView = [[UIView alloc] init];
-    headerView.backgroundColor = [UIColor clearColor];
-    return headerView;
+- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath {
+    cell.backgroundColor = [UIColor clearColor];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    static NSString *CellIdentifier = @"OrderDetail_Cell";
-    OrderDetail_Cell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-    cell=nil;
-    if (cell == nil)
+    if (tableView==CustomerTBL)
     {
-        cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+        static NSString *CellIdentifier = @"Cell";
+        UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+        cell=nil;
+        if (cell == nil)
+        {
+            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:nil];
+            cell.accessoryView = nil;
+            cell.selectionStyle = UITableViewCellSelectionStyleNone;
+        }
         
+        UILabel *titleLBL=[[UILabel alloc]initWithFrame:CGRectMake(30, 0, 300, 44)];
+        titleLBL.textColor=[UIColor colorWithRed:(132/255.0) green:(132/255.0) blue:(132/255.0) alpha:1.0];
+        //titleLBL.backgroundColor=[UIColor redColor];
+        
+        if (IS_IPHONE_4 || isIPhone5)
+        {
+            titleLBL.font=[UIFont systemFontOfSize:11.5f];
+        }
+        else
+        {
+            titleLBL.font=[UIFont systemFontOfSize:13.0f];
+        }
+        titleLBL.text=[[customerDict valueForKey:@"cname"] objectAtIndex:indexPath.row];
+        [cell addSubview:titleLBL];
+        [cell setSelectionStyle:UITableViewCellSelectionStyleNone];
+        return cell;
     }
-    
-    cell.ProductName.text=[[OrderProductDict valueForKey:@"product_name"] objectAtIndex:indexPath.section];
-    
-    cell.ProductPrice.text=[[OrderProductDict valueForKey:@"product_price"] objectAtIndex:indexPath.section];
-    
-    cell.ProductQTY.text=[[OrderProductDict valueForKey:@"receive_qty"] objectAtIndex:indexPath.section];
-    cell.ProductAmount.text=[[OrderProductDict valueForKey:@"product_total"] objectAtIndex:indexPath.section];
-    
-    [cell setSelectionStyle:UITableViewCellSelectionStyleNone];
-    return cell;
+    else
+    {
+        static NSString *CellIdentifier = @"OrderDetail_Cell";
+        OrderDetail_Cell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+        cell=nil;
+        if (cell == nil)
+        {
+            cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+            
+        }
+        
+        cell.ProductName.text=[[ProductArry valueForKey:@"name"] objectAtIndex:indexPath.section];
+        
+        cell.ProductPrice.text=[[ProductArry valueForKey:@"price"] objectAtIndex:indexPath.section];
+        
+        cell.ProductQTY.text=[[ProductArry valueForKey:@"qty"] objectAtIndex:indexPath.section];
+        
+        NSInteger totalValue=[[[ProductArry valueForKey:@"qty"] objectAtIndex:indexPath.section] integerValue]*[[[ProductArry valueForKey:@"price"] objectAtIndex:indexPath.section] integerValue];
+        cell.ProductAmount.text=[NSString stringWithFormat:@"%ld",(long)totalValue];
+        
+        [cell setSelectionStyle:UITableViewCellSelectionStyleNone];
+        return cell;
+    }
+    return 0;
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    
+    if (tableView==CustomerTBL)
+    {
+        CutomerView.hidden=YES;
+        
+        [self.view setNeedsUpdateConstraints];
+        [self.view updateConstraintsIfNeeded];
+        
+        // Take Cutomer Detail From Here.
+        [SelectCutomer_Button setTitle:[[customerDict valueForKey:@"cname"]objectAtIndex:indexPath.row ]forState:UIControlStateNormal];
+        CutomerID=[[customerDict valueForKey:@"id"]objectAtIndex:indexPath.row];
+        CutomerNameLBL.text=[[customerDict valueForKey:@"cname"]objectAtIndex:indexPath.row ];
+        CustomerAdressLBL.text=[[customerDict valueForKey:@"address"]objectAtIndex:indexPath.row ];
+        CustomerPhoneLBL.text=[[customerDict valueForKey:@"phone"]objectAtIndex:indexPath.row ];
+        NSString *city=[[customerDict valueForKey:@"city"]objectAtIndex:indexPath.row ];
+        NSString *State=[[customerDict valueForKey:@"state"]objectAtIndex:indexPath.row ];
+        NSString *Country=[[customerDict valueForKey:@"country"]objectAtIndex:indexPath.row ];
+        CustomerStateCityLBL.text=[NSString stringWithFormat:@"%@,%@,%@",city,State,Country];
+        
+        TitleTop1.constant=15;
+        TitleTop2.constant=8;
+        TitleTop3.constant=8;
+        TitleTop4.constant=8;
+        TitleHight.constant=18;
+        
+        
+        [self.view setNeedsUpdateConstraints];
+        [self.view updateConstraintsIfNeeded];
+        
+    }
 }
 
-- (IBAction)Edit_Btn_Action:(id)sender
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (tableView==CustomerTBL)
+    {
+        return 44;
+    }
+    return 50;
+    
+}
+- (IBAction)SearchProBtn_action:(id)sender
+{
+    SerachProductVW *vcr = [[UIStoryboard storyboardWithName:@"Main" bundle:nil] instantiateViewControllerWithIdentifier:@"SerachProductVW"];
+    vcr.delegate=self;
+    [self.navigationController pushViewController:vcr animated:YES];
+}
+
+- (void)ChkProductValue:(NSMutableArray *)ProductDict
+{
+    // NSLog(@"value====%@",ProductDict);
+    
+    // NSString *jsonString = [ProductDict JSONRepresentation];
+    ProductArry=[[NSMutableArray alloc]initWithArray:ProductDict];
+    
+    totalAmount=0;
+    totalQTY=0;
+    for (NSInteger jj=0; jj<ProductArry.count; jj++)
+    {
+        totalAmount=totalAmount+[[[ProductArry valueForKey:@"qty"] objectAtIndex:jj] integerValue]*[[[ProductArry valueForKey:@"price"] objectAtIndex:jj] integerValue];
+        
+        totalQTY=totalQTY+[[[ProductArry valueForKey:@"qty"] objectAtIndex:jj] integerValue];
+    }
+    
+    self.TotalQTY_LBL.text=[NSString stringWithFormat:@"Nos.%ld",(long)totalQTY];
+    self.GrantTotal_LBL.text=[NSString stringWithFormat:@"Rs.%ld",(long)totalAmount];
+    
+    NSError * err;
+    NSData * jsonData = [NSJSONSerialization dataWithJSONObject:ProductArry options:0 error:&err];
+    ProductJSONString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+    
+    [ProductTBL reloadData];
+    
+}
+- (IBAction)CreateOrderBtn_Action:(id)sender
+{
+    if ([CutomerID isEqualToString:@""])
+    {
+        [AppDelegate showErrorMessageWithTitle:@"Error!" message:@"Please Select Customer." delegate:nil];
+    }
+    else if (ProductArry.count==0)
+    {
+        [AppDelegate showErrorMessageWithTitle:@"Error!" message:@"Please Select Product." delegate:nil];
+    }
+    else
+    {
+        BOOL internet=[AppDelegate connectedToNetwork];
+        if (internet)
+        {
+            [self CreateInwardOrder];
+        }
+        else
+            [AppDelegate showErrorMessageWithTitle:@"" message:@"Please check your internet connection or try again later." delegate:nil];
+    }
+    
+    
+}
+-(void)CreateInwardOrder
 {
     
+    NSDictionary *UserSaveData=[[NSUserDefaults standardUserDefaults]objectForKey:@"LoginUserDic"];
+    NSMutableDictionary *dictParams = [[NSMutableDictionary alloc] init];
+    [dictParams setObject:Base_Key  forKey:@"key"];
+    [dictParams setObject:add_inward  forKey:@"s"];
+    
+    [dictParams setObject:CutomerID  forKey:@"vendor_id"];
+    [dictParams setObject:[UserSaveData valueForKey:@"id"] forKey:@"sales_id"];
+    
+    [dictParams setObject:[NSString stringWithFormat:@"%ld",(long)totalAmount]  forKey:@"grand_total"];
+    [dictParams setObject:[NSString stringWithFormat:@"%ld",(long)totalQTY]  forKey:@"qty"];
+    [dictParams setObject:@""  forKey:@""];
+    [dictParams setObject:ProductJSONString  forKey:@"product"];
+    
+    [CommonWS AAwebserviceWithURL:[NSString stringWithFormat:@"%@",BaseUrl] withParam:dictParams withCompletion:^(NSDictionary *response, BOOL success1)
+     {
+         [self handleInwardResponse:response];
+     }];
 }
-
+- (void)handleInwardResponse:(NSDictionary*)response
+{
+    if ([[[response objectForKey:@"ack"]stringValue ] isEqualToString:@"1"])
+    {
+        [AppDelegate showErrorMessageWithTitle:AlertTitleError message:[response objectForKey:@"ack_msg"] delegate:nil];
+        [self.navigationController popViewControllerAnimated:YES];
+    }
+    else
+    {
+        [AppDelegate showErrorMessageWithTitle:AlertTitleError message:[response objectForKey:@"ack_msg"] delegate:nil];
+    }
+}
 - (IBAction)BackBtn_Action:(id)sender
 {
     [self.navigationController popViewControllerAnimated:YES];
 }
-
-- (void)didReceiveMemoryWarning
-{
+- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
 
 
 @end
